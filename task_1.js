e851b284bf71fc1cbfdbc13c9baa8d018513e523
Айсулу <aisuluueco2009@yandex.ru>
2019-11-21 20:44:50 +0600

    Изменила задание №2, сделала функцию.

--- task_1.js
+++ task_1.js
@@ -1,8 +1,9 @@
 let myArray = ['молоко', 'пиво', 'пиво', 'молоко', 'молоко'];
 for (let i = 0; i < myArray.length; i++) { 
-if (i == 1 || i == 2) { 
+if(myArray[i] == 'пиво') { 
 console.log('Плохо'); 
 } else { 
 console.log('Хорошо'); 
-} 
 }
+}
+
--- task_2.js
+++ task_2.js
@@ -1,23 +1,13 @@
-let firstArray = [1, 5, 12, 4, 3];
-let sum = 0; 
-for (let i = 0; i < firstArray.length; i++) { 
-sum += firstArray[i] 
-} 
-let average = sum / firstArray.length; 
-console.log('Среднеарифметическое значение: ' + average);
-
-let secondArray = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-let sumSecond = 0; 
-for (let i = 0; i < secondArray.length; i++) { 
-sumSecond += secondArray[i] 
-} 
-let averageSecond = sumSecond / secondArray.length; 
-console.log('Среднеарифметическое значение: ' + averageSecond);
-
-let thirdArray = [3, 10, 17];
-let sumThird = 0; 
-for (let i = 0; i < thirdArray.length; i++) { 
-sumThird += thirdArray[i] 
-} 
-let averageThird = sumThird / thirdArray.length; 
-console.log('Среднеарифметическое значение: ' + averageThird);
+myArray = [1, 5, 12, 4, 3];
+myArraySecond = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+myArrayThird = [3, 10, 17];
+let myFunction = function(array){
+let sum = 0;
+for (let i = 0; i < array.length; i++) { 
+sum += array[i]}
+let average = sum / array.length; 
+return console.log('Среднеарифметическое значение: ' + average);
+}
+myFunction(myArray)
+myFunction(myArraySecond)
+myFunction(myArrayThird)
